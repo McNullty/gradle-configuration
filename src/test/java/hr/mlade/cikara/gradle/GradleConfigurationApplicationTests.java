c3c2a4ea4470a7df64572eafa805e698a3b537f4
Mladen Čikara <mladen.cikara@gmail.com>
2019-03-05 22:28:51 +0100

    Setting up JUnit5

--- src/test/java/hr/mlade/cikara/gradle/GradleConfigurationApplicationTests.java
+++ src/test/java/hr/mlade/cikara/gradle/GradleConfigurationApplicationTests.java
@@ -1,16 +1,13 @@
 package hr.mlade.cikara.gradle;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class GradleConfigurationApplicationTests {
+class GradleConfigurationApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 		System.out.println("From unit Test");
 	}
 
