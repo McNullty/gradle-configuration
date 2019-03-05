00ec34e5c6fc4e5dad0c1b450474ac1bdbc5a2e9
Mladen Čikara <mladen.cikara@gmail.com>
2019-03-05 23:37:05 +0100

    Setting up integration tests

---
+++ src/integration-test/java/hr/mlade/cikara/gradle/GradleConfigurationApplicationTests.java
@@ -0,0 +1,14 @@
+package hr.mlade.cikara.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("From integration Test");
+	}
+
+}
